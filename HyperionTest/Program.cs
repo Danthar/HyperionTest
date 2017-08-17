6ca2e7ff92f9bcaf34693ee4f1873b8c57047120
Arjen Smits <Deathraven163@gmail.com>
2017-08-17 16:00:10 +0200

    Initial commit

---
+++ HyperionTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace HyperionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = 
+@"akka {
+  actor {
+    serializers {
+      hyperion = ""Akka.Serialization.HyperionSerializer, Akka.Serialization.Hyperion""
+    }
+        serialization-bindings {
+            ""System.Object"" = hyperion
+        }
+    }
+}";
+
+            using (var sys = ActorSystem.Create("test"))
+            {
+
+                var a = sys.ActorOf<TestActor>();
+                a.Tell("Arjen");
+
+                Console.WriteLine("started");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class TestActor : ReceiveActor
+    {
+        public TestActor()
+        {
+            Receive<string>(_ =>
+            {
+                Console.WriteLine($"Hello {_}");
+            });
+        }
+    }
+}
